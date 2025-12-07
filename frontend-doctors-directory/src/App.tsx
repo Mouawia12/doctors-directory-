2adb2705d7c6d2de68e93104a84ab3a34bb323b5
mouawia <ymouawia10@gmail.com>
2025-12-07 18:40:37 +0100

    Describe your changes

--- frontend-doctors-directory/src/App.tsx
+++ frontend-doctors-directory/src/App.tsx
@@ -1,8 +1,35 @@
 import { RouterProvider } from 'react-router-dom'
+import { useEffect } from 'react'
 import { router } from '@/app/router'
+import { useSiteSettingsQuery } from '@/features/settings/hooks'
+
+const FaviconManager = () => {
+  const { data } = useSiteSettingsQuery()
+
+  useEffect(() => {
+    if (typeof document === 'undefined') return
+
+    const faviconUrl = data?.site_logo_url ?? '/vite.svg'
+    const existingLink = document.querySelector<HTMLLinkElement>("link[rel*='icon']") ?? document.createElement('link')
+    existingLink.type = 'image/png'
+    existingLink.rel = 'icon'
+    existingLink.href = faviconUrl
+
+    if (!existingLink.parentNode) {
+      document.head.appendChild(existingLink)
+    }
+  }, [data?.site_logo_url])
+
+  return null
+}
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <FaviconManager />
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App
